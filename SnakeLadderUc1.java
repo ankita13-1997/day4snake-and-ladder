1e92e6398ad4408858fb09ff021b23d149f1ca99
ankita13-1997 <parhiankita@gmail.com>
2021-02-02 22:54:07 +0530

    snake and ladder game started

---
+++ SnakeLadderUc1.java
@@ -0,0 +1,10 @@
+package com.JavaParactice;
+
+public class SnakeLadderUc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("snake and ladder game played with single player at start position 0");
+	}
+
+}
